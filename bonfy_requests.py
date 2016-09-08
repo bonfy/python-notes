3d682fd88570a8b32b60bcdc2cb22bb02b028be2
bonfy <foreverbonfy@163.com>
2016-09-08 22:55:41 +0800

    2016-09-07加入requests封装

---
+++ bonfy_requests.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+对 requests 的封装
+"""
+
+# 请先安装requests
+# pip install requests
+
+import random
+import requests
+from requests.exceptions import ConnectionError, Timeout
+
+
+def requests_get(url, **kwargs):
+    USER_AGENTS = (
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100 101 Firefox/22.0',
+        'Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0',
+        ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) '
+         'Chrome/19.0.1084.46 Safari/536.5'),
+        ('Mozilla/5.0 (Windows; Windows NT 6.1) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46'
+         'Safari/536.5')
+    )
+    try:
+        r = requests.get(
+            url,
+            timeout=30,
+            headers={'User-Agent': random.choice(USER_AGENTS)},
+            **kwargs
+        )
+    except ConnectionError:
+        print('Network connection failed.')
+    except Timeout:
+        print('timeout.')
+    return r
