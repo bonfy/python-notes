f3a56e87c70c2a679d2f569ab8f126b7ab617ab1
bonfy <foreverbonfy@163.com>
2016-09-21 20:44:25 +0800

    add python version 判断

--- 3_is_py_version.py
+++ 3_is_py_version.py
@@ -28,6 +28,25 @@ def is_py_version_2():
         raise RuntimeError('At least Python 3.4 is required')
 
 
+def is_py_version_3():
+    """
+    From werkzeug
+    判断python version
+    """
+    PY2 = sys.version_info[0] == 2
+
+    if PY2:
+        unichr = unichr
+        text_type = unicode
+        string_types = (str, unicode)
+        integer_types = (int, long)
+    else:
+        unichr = chr
+        text_type = str
+        string_types = (str, )
+        integer_types = (int, )
+
+
 if __name__ == '__main__':
     is_py_version()
     is_py_version_2()
